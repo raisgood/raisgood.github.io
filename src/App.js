69577252a7d5015b3e36b520bfd53fc026b6ee28
raisgood <rashadthecoder@outlook.com>
2020-12-23 14:05:23 -0600

    deploying

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import React, { useState } from 'react';
+import WorkoutForm from './components/WorkoutForm/WorkoutForm';
+import WorkoutList from './components/WorkoutList/WorkoutList';
+
+function App() {
+  const [counter, setCounter] = useState(4);
+  const [data, setData] = useState([
+  {exercise: 'Sit ups', reps: 20, key: 0},
+  {exercise: 'Bench press', reps: 12, key: 1},
+  {exercise: 'Push ups', reps: 30, key: 2},
+  {exercise: 'Burpees', reps: 20, key: 3}
+]);
+
+const handleClear = (event) => {
+  event.preventDefault();
+  setData([]);
+}
+
+const handleAdd = (exercise, reps) => {
+  const key = counter;
+  setCounter(counter + 1)
+  setData( oldData => [...oldData, {exercise, reps, key}]);
+}
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Workout App</h2>
+      </header>
+        <WorkoutForm handleClear={handleClear} 
+        handleAdd={handleAdd} />
+        <WorkoutList items={data} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WorkoutForm/WorkoutForm.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './WorkoutForm.css';
+
+export default function WorkoutForm(props) {
+    const updateData = event => {
+        event.preventDefault();
+        const workout = document.querySelector('.js-workout-item').value;
+        const reps = document.querySelector('.js-reps').value;
+        document.querySelector('.js-workout-item').value = '';
+        document.querySelector('.js-reps').value = '';
+        props.handleAdd(workout, reps);
+    }
+    return (
+        <form onSubmit={ updateData }>
+            <input type="text" 
+                name="workout-item" 
+                className="js-workout-item" 
+                placeholder="Enter workout item" />
+            <input type="number"
+                    className="js-reps"
+                    placeholder="reps" />
+            <button type="submit"> Add Work Item </button>
+            <button onClick={props.handleClear}>Clear Workouts</button>
+        </form>
+    )
+}
